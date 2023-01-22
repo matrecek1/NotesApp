31f2b99824fcc5f7f3e68dc9ffff2034b93ad399
matrecek1 <grenovec@seznam.cz>
2023-01-22 17:02:14 +0100

    why dont you work...

--- src/public/js/submitingForms.ts
+++ src/public/js/submitingForms.ts
@@ -1,9 +1,9 @@
 const form = document.querySelector('form')!
 const submitBtn = document.querySelector('.btn[type="submit"]')!
-
-form.onsubmit = () =>{
+form.addEventListener('submit', (evt =>{
     submitBtn.setAttribute('disabled', '')
     const children = submitBtn.children
     children[0].classList.remove('visually-hidden')
-}
+}))
+ 
 
