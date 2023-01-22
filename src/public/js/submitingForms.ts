ed91303193d0b24b5490402838594263881c29ad
matrecek1 <grenovec@seznam.cz>
2023-01-22 17:09:53 +0100

    UX changes

--- src/public/js/submitingForms.ts
+++ src/public/js/submitingForms.ts
@@ -1,9 +1,9 @@
 const form = document.querySelector('form')!
 const submitBtn = document.querySelector('.btn[type="submit"]')!
-form.addEventListener('submit', (evt =>{
+form.addEventListener('submit', (evt => {
     submitBtn.setAttribute('disabled', '')
     const children = submitBtn.children
     children[0].classList.remove('visually-hidden')
 }))
- 
+
 
